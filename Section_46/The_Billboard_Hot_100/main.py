fbaf4b3cc93f83f29c82ab65aa23e17a4701a20f
thinh.pham <thinhphamdn1999@gmail.com>
2025-03-27 20:58:48 +0700

    feat: update section 46

--- Section_46/The_Billboard_Hot_100/main.py
+++ Section_46/The_Billboard_Hot_100/main.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import pprint
 from dotenv import load_dotenv
 import os
 
@@ -13,22 +12,56 @@ CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
 
 
-travel_time = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"}
+travel_time = input(
+    "Which year do you want to travel to? Type the date in this format YYYY-MM-DD: "
+)
+header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+}
 
-response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{travel_time}/", headers=header)
+response = requests.get(
+    url=f"https://www.billboard.com/charts/hot-100/{travel_time}/", headers=header
+)
 response.raise_for_status()
 
 data = response.text
 
 soup = BeautifulSoup(data, "html.parser")
-songs = soup.select(selector=".o-chart-results-list-row-container .o-chart-results-list-row #title-of-a-story")
-print(songs)
+songs = soup.select(
+    selector=".o-chart-results-list-row-container .o-chart-results-list-row #title-of-a-story"
+)
+song_titles = [song.getText().strip() for song in songs]
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                               client_secret=CLIENT_SECRET,
-                                               redirect_uri=REDIRECT_URI,
-                                               scope="playlist-modify-private"))
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        scope="playlist-modify-private",
+    )
+)
 
 current_user = sp.current_user()
-pprint.pprint(current_user)
+if not current_user:
+    print("Please log in to Spotify.")
+    exit()
+
+# Create a new playlist
+user_id = current_user["id"]
+playlist = sp.user_playlist_create(
+    user=user_id, name=f"{travel_time} Billboard 100", public=False
+)
+
+if not playlist:
+    print("Failed to create a playlist.")
+    exit()
+playlist_id = playlist["id"]
+
+for song in song_titles:
+    result = sp.search(q=f"track:{song} year:{travel_time[:4]}", type="track")
+    if result and result.get("tracks") and result["tracks"].get("items"):
+        uri = result["tracks"]["items"][0]["uri"]
+        sp.playlist_add_items(playlist_id, items=[uri])
+        print(f"{song} added to your playlist.")
+    else:
+        print(f"No results found for {song}. Skipped.")
